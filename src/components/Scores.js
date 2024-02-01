cb5813b4d62aa351709a179a34ba3077704d1536
Jeneil Henry <shenry.jeneil@gmail.com>
2024-01-31 22:14:18 -0500

    new repo for student scores

---
+++ src/components/Scores.js
@@ -0,0 +1,17 @@
+function Scores({scores}){
+
+    return(
+    <div className="scores">
+
+     {scores.map((score, index) =>  
+     <li className="scores" key={index}> Score {score.date}: {score.score} </li>
+     )
+     }
+
+    </div>
+    )
+}
+export default Scores;
+ 
+ 
+ 
